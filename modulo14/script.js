366e2edc1a855d841cfe399610e90c3a3cb1cd9a
attilamonteiro <atmaload@gmail.com>
2023-10-02 15:28:20 -0300

    commit modulo05

---
+++ modulo14/script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Função para alternar abas de notícias
+    function openTab(tabId) {
+      // Esconder todos os conteúdos de abas
+      var tabContents = document.querySelectorAll('.tab-content');
+      tabContents.forEach(function(content) {
+        content.style.display = 'none';
+      });
+  
+      // Mostrar o conteúdo da aba selecionada
+      var selectedTabContent = document.getElementById(tabId);
+      if (selectedTabContent) {
+        selectedTabContent.style.display = 'block';
+      }
+    }
+  
+    // Adicione event listeners aos botões de abas
+    var tabButtons = document.querySelectorAll('.news-tab button');
+    tabButtons.forEach(function(button) {
+      button.addEventListener('click', function() {
+        var tabId = this.getAttribute('data-tab'); // Use data-tab para armazenar o ID da aba
+        openTab(tabId);
+      });
+    });
+});
